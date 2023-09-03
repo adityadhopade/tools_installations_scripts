dfbf682629596face4a3cce571ab4c9c0ab4b1b3
Aditya Dhopade <48392204+adityadhopade@users.noreply.github.com>
2023-09-03 11:46:03 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -15,3 +15,16 @@ sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo service jenkins start
 cat /var/lib/jenkins/secret/initialAdminPassword
+
+------------ For the Redhat Distro ---------------------------------
+sudo yum update
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo amazon-linux-extras install java-openjdk11
+
+sudo amazon-linux-extras install epel
+sudo yum install daemonize -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
