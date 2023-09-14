87f775af46ea156e14017d690b3959753379e197
Aditya Dhopade <48392204+adityadhopade@users.noreply.github.com>
2023-09-14 19:33:25 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -11,8 +11,23 @@ sudo systemctl status docker
 #  Allow docker to run without sudo
 sudo usermod -aG docker $USER
 newgrp docker
+------------------------------------------------------------------------------------------------
+# Install on Ubuntu 22.04
 
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
+groups
+sudo usermod -aG docker username
 
+------------------------------------------------------------------------------------------
 
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.0.zip
 unzip sonarqube-9.0.zip
