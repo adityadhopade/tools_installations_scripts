74385db721c51fa1a5ac5e343bea14ac8482b554
Aditya Dhopade <48392204+adityadhopade@users.noreply.github.com>
2023-09-14 15:18:58 +0530

    Update jenkins.sh for ubuntu

--- jenkins.sh
+++ jenkins.sh
@@ -47,3 +47,20 @@ sudo systemctl status jenkins
 
 
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+------------------------------------------ UBUNTU --------------------------------
+sudo apt-get update
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo apt-get upgrade
+sudo dnf install java-11-amazon-corretto -y
+sudo apt install jenkins -y
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins.service
+sudo systemctl status jenkins
