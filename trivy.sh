7ed90fbf9ea57bef89dd4f1891bab485c1e36349
Aditya Dhopade <48392204+adityadhopade@users.noreply.github.com>
2023-09-14 20:35:42 +0530

    Update trivy.sh
    Updated Trivy with the New Commands and the script

--- trivy.sh
+++ trivy.sh
@@ -5,4 +5,20 @@ wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-k
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
 sudo apt-get install trivy
+---------------------------------------------------------------------------
 
+
+--------------------- IMPORTANT TRIVY COMMANDS -----------------------------------
+
+
+trivy image imagename
+
+trivy fs --security-checks vuln,config   Folder_name_OR_Path
+
+trivy image --severity HIGH,CRITICAL image_name
+
+trivy image -f json -o results.json image_name
+
+trivy repo repo-url
+
+trivy k8s --report summary cluster
