6e5618523baafcee4bc652f99b8db00710497a64
Aditya Dhopade <48392204+adityadhopade@users.noreply.github.com>
2023-09-14 13:40:50 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -28,3 +28,20 @@ sudo yum install daemonize -y
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
+
+
+-------------------------AMI AMAZON LINUX 3 -----------------------------------------
+
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-11-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins    
+sudo systemctl status jenkins
+
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
