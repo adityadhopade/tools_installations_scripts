6d885be323ef0223fe8b6c4d845e6c10f4f17bf7
Aditya Dhopade <48392204+adityadhopade@users.noreply.github.com>
2023-09-14 15:08:47 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -1,5 +1,5 @@
 ----------------------Trivy Installation Ubuntu----------------------
-
+---------------------- NOT WORKS IN AMAZON LINUX ----------------------
 sudo apt-get install wget apt-transport-https gnupg lsb-release
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
