06fee439ca5f79e1594fecdaa672691230324c96
Aditya Dhopade <48392204+adityadhopade@users.noreply.github.com>
2023-09-14 19:03:40 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -50,17 +50,15 @@ sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 
 ------------------------------------------ UBUNTU --------------------------------
-sudo apt-get update
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo apt-get upgrade
-sudo dnf install java-11-amazon-corretto -y
-sudo apt install jenkins -y
+sudo apt install openjdk-11-jdk -y
+java --version
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins
-sudo systemctl start jenkins.service
+sudo apt install jenkins -y
+sudo systemctl start jenkins && sudo systemctl enable jenkins
 sudo systemctl status jenkins
