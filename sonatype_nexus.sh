48df59622b9bc2a213d353c5017d86f0106e2b96
Aditya Dhopade <48392204+adityadhopade@users.noreply.github.com>
2023-09-18 10:48:05 +0530

    Update sonatype_nexus.sh

--- sonatype_nexus.sh
+++ sonatype_nexus.sh
@@ -4,7 +4,7 @@
 
 wget https://download.sonatype.com/nexus/3/nexus-3.60.0-02-unix.tar.gz
 tar -xvzf nexus-3.60.0-02-unix.tar.gz
-cd nexus-3.60.0-02
+cd nexus-3.60.0-02/bin
 ./nexus start
 ./nexus status
 
