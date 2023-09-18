2b34949a3aabf9b3e4b5bddbf22c164a67d07d82
Aditya Dhopade <48392204+adityadhopade@users.noreply.github.com>
2023-09-18 10:45:29 +0530

    Create sonatype_nexus.sh

---
+++ sonatype_nexus.sh
@@ -0,0 +1,11 @@
+#TO install nexus we need to install java first
+
+# to install nexus only
+
+wget https://download.sonatype.com/nexus/3/nexus-3.60.0-02-unix.tar.gz
+tar -xvzf nexus-3.60.0-02-unix.tar.gz
+cd nexus-3.60.0-02
+./nexus start
+./nexus status
+
+## ACCESS USING THE PORT 8081  => IP ADDRESS:8081
